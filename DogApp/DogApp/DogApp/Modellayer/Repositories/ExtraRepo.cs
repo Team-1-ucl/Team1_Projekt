70bc34aa0e8d64055e99932cb5da104186575b1d
BenjaminBDC <bdch54128@edu.ucl.dk>
2024-03-04 16:32:46 +0100

    setup Repos

--- DogApp/DogApp/DogApp/Modellayer/Repositories/ExtraRepo.cs
+++ DogApp/DogApp/DogApp/Modellayer/Repositories/ExtraRepo.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using DogApp.Data;
+using DogApp.Modellayer.Model.Items;
+using DogRallyApp.ModelLayer.Repositories;
+
+namespace DogApp.Modellayer.Repositories;
+
+public class ExtraRepo : GenericRepository<Extra>
+{
+    public ExtraRepo(DataContext context) : base(context)
+    {
+    }
+}
